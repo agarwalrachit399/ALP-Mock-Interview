874022bc510554a0084bebb0fa57f018f9dac0ab
Rachit Agarwal <agarwalrachit399@gmail.com>
2025-06-20 18:43:21 -0700

    Secured JWT Token

--- backend/auth_service/app/core/config.py
+++ backend/auth_service/app/core/config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-JWT_SECRET = os.getenv("JWT_SECRET", "secret123")
+JWT_SECRET = os.getenv("JWT_SECRET_KEY")
 JWT_ALGORITHM = "HS256"
 JWT_EXPIRY_MINUTES = 60
